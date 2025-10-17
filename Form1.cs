734fbd1c7ec4fc60e33809c011b2825136ed4568
Duncan Mullier <D.Mullier@leedsbeckett.ac.uk>
2025-10-17 15:55:42 +0100

    Example 2 catching a number format exception

--- Form1.cs
+++ Form1.cs
@@ -14,9 +14,17 @@ namespace ExceptionsExample
         public void doSomething()
         {
             int num1, num2, result;
-
-            num1 = Int32.Parse(textBox1.Text);
-            num2 = Int32.Parse(textBox2.Text);
+            try
+            {
+                num1 = Int32.Parse(textBox1.Text);
+                num2 = Int32.Parse(textBox2.Text);
+            }
+            catch (FormatException)
+            {
+                textBox1.Text = "Input format is incorrect. Please enter valid integers.";
+                return;
+            }
+           
             result = num1 / num2;
 
             textBox1.Text = "result = " + result.ToString();
